940cb0be7b3a060167d4c1d387213d07749cadc3
Lucas Macedo <63942069+seilamacedo@users.noreply.github.com>
2021-12-20 16:33:05 -0300

    Update mail.js

--- mail.js
+++ mail.js
@@ -18,58 +18,42 @@
   firebase.initializeApp(firebaseConfig);
   const analytics = getAnalytics(app);
 
-  // reference your database
-  var contactFormDB = firebase.database().ref("contactForm");
-  
-  document.getElementById("contactForm").addEventListener("submit", submitForm);
-  
-  function submitForm(e) {
-    e.preventDefault();
-  
-    var name = getElementVal("name");
-    var sobrenome = getElementVal("sobrenome");
-    var email = getElementVal("email");
-    var zap = getElementVal("zap");
-    var problema = getElementVal("problema");
-    var alinhamento1 = getElementVal("alinhamento1");
-    var alinhamento2 = getElementVal("alinhamento2");
-    var alinhamento3 = getElementVal("alinhamento3");
-    var alinhamento4 = getElementVal("alinhamento4");
-    var alinhamento5 = getElementVal("alinhamento5");
-    var alinhamento6 = getElementVal("alinhamento6");
-  
-    saveMessages(name, sobrenome, email, zap, problema, alinhamento1, alinhamento2, alinhamento3, alinhamento4,  alinhamento5, alinhamento6);
-  
-    //   enable alert
-    document.querySelector(".alert").style.display = "block";
-  
-    //   remove the alert
-    setTimeout(() => {
-      document.querySelector(".alert").style.display = "none";
-    }, 3000);
-  
-    //   reset the form
-    document.getElementById("contactForm").reset();
-  }
-  
-  const saveMessages = (name, sobrenome, email, zap, problema, alinhamento1, alinhamento2, alinhamento3, alinhamento4,  alinhamento5, alinhamento6) => {
-    var newContactForm = contactFormDB.push();
-  
-    newContactForm.set({
-    name:name,
-    sobrenome: sobrenome,
-    email: email,
-    zap: zap,
-    problema: problema,
-    alinhamento1: alinhamento1,
-    alinhamento2: alinhamento2,
-    alinhamento3: alinhamento3,
-    alinhamento4: alinhamento4,
-    alinhamento5: alinhamento5,
-    alinhamento6: alinhamento6,
-    });
-  };
-  
-  const getElementVal = (id) => {
-    return document.getElementById(id).value;
-  };
+// reference your database
+var contactFormDB = firebase.database().ref("contactForm");
+
+document.getElementById("contactForm").addEventListener("submit", submitForm);
+
+function submitForm(e) {
+  e.preventDefault();
+
+  var name = getElementVal("name");
+  var emailid = getElementVal("emailid");
+  var msgContent = getElementVal("msgContent");
+
+  saveMessages(name, emailid, msgContent);
+
+  //   enable alert
+  document.querySelector(".alert").style.display = "block";
+
+  //   remove the alert
+  setTimeout(() => {
+    document.querySelector(".alert").style.display = "none";
+  }, 3000);
+
+  //   reset the form
+  document.getElementById("contactForm").reset();
+}
+
+const saveMessages = (name, emailid, msgContent) => {
+  var newContactForm = contactFormDB.push();
+
+  newContactForm.set({
+    name: name,
+    emailid: emailid,
+    msgContent: msgContent,
+  });
+};
+
+const getElementVal = (id) => {
+  return document.getElementById(id).value;
+};
